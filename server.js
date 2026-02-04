6d8809966b0322cd56154c3286da09a02af1bb02
TheSkibidiMewinger <test.for.exploring.around@gmail.com>
2026-02-04 21:02:06 +0700

    six seventh patch tryna make it work on vercel fr this time

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import cors from "cors";
 import { GoogleGenAI } from "@google/genai";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 
 app.post("/api/exam", async (req, res) => {
     try {
